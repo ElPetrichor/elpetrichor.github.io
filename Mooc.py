da3e5780b419dea752dc4c75d1d4242a70333f35
ElPetrichor <47576775+ElPetrichor@users.noreply.github.com>
2025-02-18 09:13:42 +0100

    Update Mooc.py

--- Mooc.py
+++ Mooc.py
@@ -23,42 +23,9 @@ if __name__ == "__main__":
 # Sudoku: check row
 
 def row_correct (sudoku:list, row_no:int):
-  one = 0
-  two = 0
-  three = 0
-  four = 0
-  five = 0
-  six = 0
-  seven = 0
-  eight = 0
-  nine = 0
-  zero = 0
-  for row in sudoku:
-    for item in row:
-      if item == 1:
-        one += 1
-      if item == 2:
-        two += 1
-      if item == 3:
-        three += 1
-      if item == 4:
-        four += 1
-      if item == 5:
-        five +=1
-      if item == 6:
-        six += 1
-      if item == 7:
-        seven += 1
-      if item == 8:
-        eight += 1
-      if item == 9:
-        nine +=1
-      if item == 0:
-        zero +=1
-  if one == 1 and two == 1 and three == 1 and four == 1 and five == 1 and six == 1 and seven == 1 and eight == 1 and nine == 1 and zero ==1:
-    return True
-  else:
-    return False
+  row = sudoku [row_no]
+  numbers = [num for num in row if num != 0]
+  return len(numbers) == len(set(numbers))
 
 if __name__ == "__main__":
  print(row_correct(sudoku, 0))
@@ -67,29 +34,51 @@ if __name__ == "__main__":
 # Sudoku: Check column:
 
 def column_correct (sudoku:list,column_no:int):
-  one = 0
-  two = 0
-  three = 0
-  four = 0
-  five = 0
-  six = 0
-  seven = 0
-  eight = 0
-  nine = 0
-  zero = 0
-  for row in sudoku:
-    if row[0] == 0:
-      zero += 1
-    if row[1] == 1:
-      zero += 1
-    if row[0] == 2:
-      zero += 1
-    if row[0] == 3:
-      zero += 1
-    if row[0] == 0:
-      zero += 1
+  column = [row[column_no] for row in sudoku]
+  numbers = [num for num in column if num != 0]
+  return len(numbers) == len(set(numbers))
     
-      
+# Sudoku: check block:
+
+def block_correct (sudoku: list, row_no:int, column_no:int):
+  numbers = []
+  for i in range(3):
+    for j in range (3):
+      num = sudoku[row_no+i][column_no+j]
+      if num != 0:
+        if num in numbers:
+          return False
+        numberrs.append(num)
+  return True
+
+if __name__ == "__main__":
+  print(block_correct(sudoku, 0, 0))
+
+
+#Sudoku: check grid:
+
+def sudoku_grid_correct (sudoku:list):
+  if row_correct () == True and column_correct () == True and block_correct () == True:
+    return True
+  else:
+    return False
+
+if __name__ == "__main__":
+  sudoku1 = [
+  [9, 0, 0, 0, 8, 0, 3, 0, 0],
+  [2, 0, 0, 2, 5, 0, 7, 0, 0],
+  [0, 2, 0, 3, 0, 0, 0, 0, 4],
+  [2, 9, 4, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 7, 3, 0, 5, 6, 0],
+  [7, 0, 5, 0, 6, 0, 4, 0, 0],
+  [0, 0, 7, 8, 0, 3, 9, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 3],
+  [3, 0, 0, 0, 0, 0, 0, 0, 2]
+]
+
+print(sudoku_grid_correct(sudoku1))
+
+
 
 
 
